4f23a5f8a764a1fc8f417bea92f6b8470c3be690
Nida Yucedal <yucedalnida@gmail.com>
2022-11-08 00:20:21 +0300

    new

--- src/test/java/post_request/Post06.java
+++ src/test/java/post_request/Post06.java
@@ -6,8 +6,10 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import pojos.DummyRestApiDataPojo;
 import pojos.DummyRestApiPojo;
+import utils.ObjectMapperUtils;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Post06 extends DummyRestApiBaseUrl {
 
@@ -78,6 +80,17 @@ public class Post06 extends DummyRestApiBaseUrl {
         Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
         response.prettyPrint();
 
+
+        //Do Assertion
+        DummyRestApiPojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), DummyRestApiPojo.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getEmployee_name(), actualData.getData().getEmployee_name());
+        assertEquals(expectedData.getEmployee_salary(), actualData.getData().getEmployee_salary());
+        assertEquals(expectedData.getEmployee_age(), actualData.getData().getEmployee_age());
+        assertEquals(expectedData.getProfile_image(), actualData.getData().getProfile_image());
+        // assertEquals("Successfully! Record has been added.",actualData.getMessage()); ==> Hard Codding
 
 
     }
