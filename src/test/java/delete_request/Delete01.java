a73c0c3f68d488cfb0df80338d5c8486844a0920
Nida Yucedal <yucedalnida@gmail.com>
2022-11-07 12:49:39 +0300

    pojo

--- src/test/java/delete_request/Delete01.java
+++ src/test/java/delete_request/Delete01.java
@@ -35,7 +35,7 @@ public class Delete01 extends JsonplaceholderBaseUrl {
         Response response = given().spec(spec).when().delete("/{first}/{second}");
 
 
-        //Do Asserti
+        //Do Assertio
         Map actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), HashMap.class);
 
 
