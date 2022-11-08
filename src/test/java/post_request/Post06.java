bb19cd7fc6df471907245066e6958244c9da75ef
Nida Yucedal <yucedalnida@gmail.com>
2022-11-08 19:23:27 +0300

    new

--- src/test/java/post_request/Post06.java
+++ src/test/java/post_request/Post06.java
@@ -92,6 +92,5 @@ public class Post06 extends DummyRestApiBaseUrl {
         assertEquals(expectedData.getProfile_image(), actualData.getData().getProfile_image());
         // assertEquals("Successfully! Record has been added.",actualData.getMessage()); ==> Hard Codding
 
-
     }
 }
