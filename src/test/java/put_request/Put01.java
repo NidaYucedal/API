5b2ae5190f6e36e8d083743cf8bb11c579db6bc7
Nida Yucedal <yucedalnida@gmail.com>
2022-11-16 16:22:21 +0300

    new

--- src/test/java/put_request/Put01.java
+++ src/test/java/put_request/Put01.java
@@ -56,7 +56,5 @@ I send PUT Request to the Url
         assertEquals(expectedData.get("title"), actualData.get("title"));
         assertEquals(expectedData.get("userId"), actualData.get("userId"));
 
-
-
     }
 }
