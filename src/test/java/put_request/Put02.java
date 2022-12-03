ec21f455ed7b04b963eb6a0ae5df73741286d119
Nida Yucedal <yucedalnida@gmail.com>
2022-12-03 12:34:49 +0300

    new

--- src/test/java/put_request/Put02.java
+++ src/test/java/put_request/Put02.java
@@ -89,5 +89,7 @@ And
         assertEquals(data.getEmployee_salary(),actualData.getData().getEmployee_salary());
         assertEquals(data.getProfile_image(),actualData.getData().getProfile_image());
 
+
+
     }
 }
