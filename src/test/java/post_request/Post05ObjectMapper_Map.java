d6830491c13cf33b69883fb8134ec1aec24cd466
Nida Yucedal <yucedalnida@gmail.com>
2022-12-16 18:42:06 +0300

    update

--- src/test/java/post_request/Post05ObjectMapper_Map.java
+++ src/test/java/post_request/Post05ObjectMapper_Map.java
@@ -66,6 +66,5 @@ public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
         assertEquals(expectedData.get("userId"), actualData.get("userId"));
         assertEquals(expectedData.get("title"), actualData.get("title"));
 
-
     }
 }
